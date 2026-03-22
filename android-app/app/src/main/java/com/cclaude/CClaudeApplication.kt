99545c738b6ba34d8c5e0703402a0e094c8c52be
navy <2603451187@qq.com>
2026-03-22 12:35:12 +0800

    Add android-app/app/src/main/java/com/cclaude/CClaudeApplication.kt

---
+++ android-app/app/src/main/java/com/cclaude/CClaudeApplication.kt
@@ -0,0 +1,10 @@
+package com.cclaude
+
+import android.app.Application
+
+class CClaudeApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        // Initialize any app-wide services here
+    }
+}
